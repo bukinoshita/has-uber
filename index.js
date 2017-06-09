761eb71ff6b27c50ad22e8da2883b45c376d9012
Bu Kinoshita <bukinoshita@gmail.com>
2017-06-09 13:24:16 -0400

    Improve finding places (https://github.com/bukinoshita/has-uber/issues/2)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const got = require('got')
 const hasUber = place => {
   return got('https://www.uber.com/api/cities')
     .then(cities => JSON.parse(cities.body).filter(city => {
-      if (city.slug === place) {
+      if (city.slug === place.toLowerCase()) {
         return true
       }
 
